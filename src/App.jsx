5cd2b88c0debccaf7befa1ea9cd0acd56a8084c1
WebWithAstha <lodhiastha85@gmail.com>
2024-07-12 22:24:24 +0530

    boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-[#ffdde6] text-[#ef99b1] flex items-center justify-center'>
+      <h1 className='text-8xl font-black uppercase'>Astha Lodhi</h1>
+
+    </div>
+  )
+}
+
+export default App
