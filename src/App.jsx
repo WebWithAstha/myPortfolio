ecee39e40feafb10d8833e7677a9e6c5cd644c68
WebWithAstha <lodhiastha85@gmail.com>
2024-07-16 22:32:08 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import Home from './components/Home'
+import Nav from './components/Nav'
 const App = () => {
   return (
-    <div className='w-full h-screen bg-[#ffdde6] text-[#ef99b1] flex items-center justify-center'>
-      <h1 className='text-8xl font-black uppercase'>Astha Lodhi</h1>
-
-    </div>
+    <>
+    <Nav/>
+    <Home/>
+    </>
   )
 }
 
