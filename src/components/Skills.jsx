f97777d70e8076098e76014c3865f5097157a44a
WebWithAstha <lodhiastha85@gmail.com>
2024-07-18 13:18:31 +0530

    skill section completed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,71 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import chick from '../assets/images/gifs/chick.gif'
 import knife from '../assets/images/gifs/knife.gif'
+import cloud from '../assets/images/gifs/cloud.gif'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { color, motion } from 'framer-motion'
+import Trial from './projectComponent/Trial'
+
+// motion
+
+const visible ={
+    scale:2,
+    color:"red"
+}
 
 
 const Skills = () => {
-    const skill1 = ["React", "Framer Motion", "Tailwind Css", "Css", "Html", "Javascript", "Next.js"]
-    const skill3 = [
-        "Node.js",
-        "Express.js",
-        "MongoDB",
-        "Razorpay",
-        "Nodemailer",
-        "Figma"
-    ];
-    const skill2 = [
-        "VS Code",
-        "Postman",
-        "Git",
-        "GitHub",
-        "Redux Toolkit",
-        "Context API",
-        "props drilling",
-        "RESTful APIs",
-        "AJAX",
-        "React Routing",
-        "Infinite Scrolling"
-    ];
+    const skillRef = useRef(null)
 
-    const stateMan = ["Redux Toolkit", "Context API"]
+    useGSAP(() => {
+        gsap.to("body", {
+            backgroundColor: "#d9f99d",
+            scrollTrigger: {
+                scroller: "body",
+                trigger: skillRef.current,
+                start: "top 1%",
+                end: "top 0%",
+                // markers:true,
+                // scrub:2,
+            }
+
+        })
+    })
+
+
+
     return (
-        <div className='shrink-0 w-full h-screen py-0 flex lg:px-40 flex-col'>
-            <div className="text-6xl lg:mr-20 mb-20">
-                <h1 className=''>What skills <br /> do I bring to the table?
-                    <img className='h-24 translate-y-2 -scale-x-100 inline-block ' src={knife} alt="" />
-                </h1>
-            </div>
-            <div className="cont flex flex-col">
-                <div className="h-[60vh] flex flex-col justify-between ">
-                    <div className="w-full h-full flex-col flex gap-10">
-                        <div className="w-[40rem]">
-                            <div className="flex text-[#d8ebf4] font-semibold  flex-wrap">
-                                {skill1.map((tech, index) => (
-                                    <div key={index} className="px-8 py-3 mb-[1px] bg-[#1a535c]/[.5] text-2xl">{tech}</div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full h-full flex-col flex items-center gap-10 mt-4">
-                        <div className="w-[40rem]">
-                            <div className="flex text-[#d8ebf4] font-semibold  flex-wrap">
-                                {skill2.map((tech, index) => (
-                                    <div key={index} className="px-8 py-3 mb-[1px] bg-[#1a535c]/[.5] text-2xl">{tech}</div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full h-full flex-col flex items-end gap-10 mt-4">
-                        <div className="w-[40rem]">
-                            <div className="flex text-[#d8ebf4] font-semibold  flex-wrap">
-                                {skill3.map((tech, index) => (
-                                    <div key={index} className="px-8 py-3 mb-[1px] bg-[#1a535c]/[.5] text-2xl">{tech}</div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div ref={skillRef} className='shrink-0 relative w-full md:pt-60 pt-32 pb-40 min-h-screen py-0 flex flex-col'>
+            <h1 className='md:text-9xl text-6xl md:mb-32 mb-10 font-[Chillax] lg:px-40 px-10 '>What
+                <span className='relative z-[0] mx-2 font-[Bonny]'> skills</span> <br /> do I bring to the table?
+                <img className='md:h-24 translate-y-2 h-16 -scale-x-100 inline-block mix-blend-darken' src={knife} alt="" />
+            </h1>
+            <Trial/>
         </div>
     )
 }
---
+++ src/components/projectComponent/Trial.jsx
@@ -0,0 +1,115 @@
+import { motion, useMotionValue, useSpring } from 'framer-motion'
+import React, { useState } from 'react'
+import { distance } from "@popmotion/popcorn";
+
+
+
+
+const grid = [
+    [],
+    [
+        
+        "Figma",
+        "Node.js",
+        "MongoDB",
+        "Razorpay",
+        "Express.js",
+    ],
+    [
+        "React",
+        "Css",
+        "Html",
+        "Framer Motion",
+        "Tailwind Css",
+
+    ], [
+
+        "Git",
+        "Javascript",
+        "VS Code",
+        "Next.js",
+
+    ], [
+        "GitHub",
+        "Postman",
+        "AJAX",
+        "Context API",
+        "RESTful APIs",
+    ]
+];
+const gap = 0;
+
+const Square = ({ active, setActive, colIndex, rowIndex, x, y }) => {
+    const isDragging = colIndex === active.col && rowIndex === active.row;
+    const diagonalIndex = (360 / 6) * (colIndex + rowIndex);
+    const d = distance(
+        { x: active.col, y: active.row },
+        { x: colIndex, y: rowIndex }
+    );
+    const springConfig = {
+        stiffness: Math.max(700 - d * 120, 0),
+        damping: 20 + d * 5
+    };
+    const dx = useSpring(x, springConfig);
+    const dy = useSpring(y, springConfig);
+
+    return (
+        <motion.div
+            drag
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }}
+            dragElastic={1}
+            onDragStart={() => setActive({ row: rowIndex, col: colIndex })}
+            style={{
+                x: isDragging ? x : dx,
+                y: isDragging ? y : dy,
+                zIndex: isDragging ? 1 : 0
+            }}
+            animate={{ scale: 1.1 }}
+            className="md:px-20 h-max px-4 cursor-pointer md:py-10 py-3 text-[#d8ebf4] font-semibold mb-[1px] bg-[#1a535c]/[.5] md:text-4xl text-2xl"
+        >
+            {grid[rowIndex][colIndex]}
+        </motion.div>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+const Trial = () => {
+
+    const [active, setActive] = useState({ row: 0, col: 0 });
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+
+
+    return (
+        <div className="flex justify-center items-center">
+            <div className='w-[80vw] h-max flex justify-center items-center flex-wrap '>
+                {grid.map((row, rowIndex) =>
+                    row.map((_item, colIndex) => (
+                        <Square
+                            x={x}
+                            y={y}
+                            active={active}
+                            setActive={setActive}
+                            rowIndex={rowIndex}
+                            colIndex={colIndex}
+                            key={rowIndex + colIndex}
+                        />
+                    ))
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Trial
