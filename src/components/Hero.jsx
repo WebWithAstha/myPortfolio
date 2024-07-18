bdf7b62173e6310bbcea7678634e99549b6ccfa1
WebWithAstha <lodhiastha85@gmail.com>
2024-07-18 13:24:11 +0530

    home done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,23 +3,45 @@ import cloud from '../assets/images/gifs/cloud.gif'
 import chick from '../assets/images/gifs/chick.gif'
 import brush from '../assets/images/gifs/brush.gif'
 import wind from '../assets/images/gifs/4.png'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 
 const Hero = () => {
+
+  // gsap.registerPlugin(ScrollTrigger)
+  
+useGSAP(()=>{
+  gsap.to('p',{
+    rotate:-1,
+    scrollTrigger:{
+      trigger:'p',
+      scroller:"body",
+      start:"top 20%",
+      end:"top 0%",
+      scrub:true,
+      // markers:true,
+      
+    }
+  })
+},[])
+
   return (
-    <div className="content shrink-0 relative w-full flex flex-col items-center justify-center ">
-        <p className='text-7xl font-[Chillax] relative lg:w-[77%]'>
-        <img className='w-80 absolute right-[0%] top-0 -translate-y-[84%] z-0' src={cloud} alt="" />
-          <span className='relative z-[9]'>
+    <div className="content shrink-0 relative w-full overflow-hidden  flex flex-col md:px-40 px-10 justify-center">
+        <p className='md:text-8xl text-6xl font-[Chillax] relative md:w-[80%]'>
+        <img className='md:w-[35rem] w-32 absolute md:left-[78%] left-[50%] md:top-0 bottom-0 translate-y-[40%] md:-translate-y-[20%] z-0 mix-blend-darken' src={cloud} alt="" />
+          <span className='relative z-[9] '>
           I'm a 
           </span>
-          <span className='font-black relative z-[0] mx-2'> MERN
-          <img className='absolute w-96 z-[-1] left-0  top-0 translate-y-[70%]' src={brush} alt="" />
+          <span className=' relative inline-block z-[0] ml-4 my-2 md:my-0 font-[Bonny] text-9xl font-semibold'> mern
+          {/* <img className='absolute w-96 z-[-1] left-0  top-0 translate-y-[70%]' src={brush} alt="" /> */}
           </span> 
-          <span className='relative ml-4'>
-           Developer dedicated to bringing your vision to life.
+          <span className='relative md:ml-4 '>
+           Developer !! turning your vision into <br /> reality.
           </span>
         </p>
-        <button className='self-start ml-[15rem] mt-4'>Get resume</button>
+        <button className='self-start mt-4'>Get resume</button>
         
       </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,34 @@ import React from 'react'
 import Hero from './Hero'
 import Horizontal from './Horizontal'
 import Skills from './Skills'
-import Projects from './Projects'
+import Projects from './projectComponent/Projects'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import Trial from './projectComponent/Trial'
+import VerticalBar from './projectComponent/VerticalBar'
+import Heading from './projectComponent/Heading'
+
 
 const Home = () => {
+
+
   return (
     
-    <div className='w-full h-screen bg-[#d8ebf4] text-[#1A535C] px-20 font-mono pb-40'>
+    <div className='w-full overflow-x-hidden min-h-screen text-[#1A535C] font-mono'>
    
-      <div className="h-screen flex items-center">
+      <div className="md:py-20 md:pt-60 pt-32 flex items-center">
       <Hero/>
       </div>
       <Skills/>
+      {/* <Trial/> */}
       <Projects/>
+      <div id='horizontal_parent' className='w-full min-h-screen h-screen overflow-x-hidden relative flex'>
+        <VerticalBar id={"bar1"} title={"fullstack"} />
+        <VerticalBar id={"bar1"} title={"backend"} />
+        <VerticalBar id={"bar1"} title={"frontend"} />
+        <Heading id={"prohead"} reverse={true} />
+    </div>
+      {/* <Horizontal/> */}
 
 
     </div>
