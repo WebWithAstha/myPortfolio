a95859c4a99b75a2e7282d90baa100cd3c6cc269
WebWithAstha <lodhiastha85@gmail.com>
2024-07-22 23:02:00 +0530

    color changed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,5 @@
 import React, { useRef } from 'react'
-import chick from '../assets/images/gifs/chick.gif'
 import knife from '../assets/images/gifs/knife.gif'
-import cloud from '../assets/images/gifs/cloud.gif'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
@@ -35,7 +33,7 @@ const Skills = () => {
 
 
     return (
-        <div ref={skillRef} className='shrink-0 relative w-full md:pt-32 pt-32 pb-40 min-h-screen py-0 flex flex-col'>
+        <div ref={skillRef} className='shrink-0 bg-[#e8e8e3] relative w-full md:pt-32 pt-32 pb-40 min-h-screen py-0 flex flex-col'>
             <h1 className='md:text-9xl text-6xl md:mb-32 mb-10 font-[Chillax] lg:px-40 px-10 '>What
                 <span className='relative z-[0] mx-2 font-[Bonny]'> skills</span> <br /> do I bring to the table?
                 <img className='md:h-24 translate-y-2 h-16 -scale-x-100 inline-block mix-blend-darken' src={knife} alt="" />
