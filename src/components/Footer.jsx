334915ab60d0f7c1119cce6e9ba7117f89c25335
WebWithAstha <lodhiastha85@gmail.com>
2024-07-26 23:12:14 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,22 @@ import underline from '../assets/images/underline.png'
 
 const Footer = () => {
     return (
-        <div className='px-40'>
-            <div className="top flex justify-center items-center flex-col py-20">
-                <p className='text-2xl'>Have any project? 
-                Looking for an edge over the competition?</p>
-                <h1 className='text-[10vw] relative leading-none text-center font-bold'>Get In Touch
-                    <img className='w-full absolute bottom-0 translate-y-[65%] scale-110' src={underline} alt="" />
-                </h1>
+        <div className='px-20 py-8'>
+            <h1 className='text-[6vw]'>Drop by a hi!!</h1>
+          <img className="arrow w-[10vw] h-max invert shrink-0 rotate-45 ml-[38vw]" src="   https://cdn-icons-png.flaticon.com/512/17426/17426390.png " alt="" />
+          <div className="border border-purple-400 rounded-full w-max ml-auto overflow-hidden">
+            <input className='outline-none w-[30vw] px-[2vw] py-4 text-[2vw] text-purple-400 bg-transparent' placeholder='Tell me something' type="text" />
+            <button className='text-[2vw] uppercase px-10 py-4 text-center font-bold font-[bespoke] text-black bg-purple-400'>Send</button>
+          </div>
+          <div className="border rounded-full my-20"></div>
+
+            <div className="top flex mt-10 items-end gap-6">
+                
+                <h1 className='text-[10vw] relative leading-[.76] text-center font-bold font-[bespoke] w-max'>Reach me</h1>
+                <div className="">
+                    <h2 className='text-[1.8vw] mb-2 uppercase leading-none'>Linkedin</h2>
+                    <h2 className='text-[1.8vw] uppercase leading-none'>GitHub</h2>
+                </div>
             </div>
 
         </div>
---
+++ src/components/animations/Animation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Animation = () => {
+    return (
+        <div className='px-20 my-16 pt-20'>
+            
+            <p className='text-[2vw] leading-relaxed'>
+                <img className='inline-block w-16 mr-2' src="https://cdn-icons-png.flaticon.com/512/1094/1094615.png " alt="" />
+                Well, it’s time to wrap things up. <br /> You’ve browsed through my portfolio, so it’s time to say adios. <br /> Just kidding! You can't get rid of me that easily! <br /> Let's definitely stay in touch and cook up some awesome projects together. </p>
+            <h1 className='hidden text-[3vw] w-[30vw] ml-32 leading-none'>Wanna see some cool animations</h1>
+            <img className="hidden arrow w-[10vw] h-max invert shrink-0 rotate-45 mx-auto" src="https://cdn-icons-png.flaticon.com/512/17426/17426390.png " alt="" />
+
+        </div>
+    )
+}
+
+export default Animation
